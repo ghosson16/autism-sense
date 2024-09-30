a4361f3af0b830e03d8852d0ba94c97ec71898bb
ghosson16 <ghossonmoafa2002@gmail.com>
2024-10-01 00:34:40 +0300

    update zoom api key

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/autism-sense/',  // Set this to your GitHub Pages repo name
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['@zoom/videosdk'], // Externalize the Zoom Video SDK
+    },
+  },
+});
