c4e6b340cb7b7cdf671102681afdbac975e96d02
rareemi <121254688+rareemi@users.noreply.github.com>
2024-10-18 15:13:01 +0300

    Revert "route added"
    This reverts commit c281567c8d6f7bd0fc91b46e29646065290715bf.

--- Backend/routes/transcriptionRoutes.js
+++
@@ -1,9 +0,0 @@
-// Backend/routes/transcriptionRoutes.js
-const express = require('express');
-const { handleTranscription } = require('../controllers/transcriptionController');
-
-const router = express.Router();
-
-router.post('/transcribe', handleTranscription);
-
-module.exports = router;
