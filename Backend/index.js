1b843eb22fd3d5b2bf418b00ed34068408cedafc
ghosson16 <ghossonmoafa2002@gmail.com>
2024-10-20 09:55:15 +0300

    update index

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,15 @@
 // Backend/index.js
 require('dotenv').config();
 const express = require('express');
+const crypto = require('crypto');
 const connectDB = require('./config/db.cjs');
 const cors = require('cors');
 const sessionMiddleware = require('./middleware/sessionMiddleware.cjs');
 
 const authRoutes = require('./routes/authRoutes.cjs');
 const childRoutes = require('./routes/childRoutes.cjs');
+const roomRoutes = require("./routes/roomRoutes.cjs");
+const emotionDetectionRoutes = require('./routes/emotionDetectionRoutes.cjs');
 const deepgramRoutes = require('./routes/deepgramRoutes.cjs'); // Add deepgram routes
 
 const app = express();
@@ -24,6 +27,8 @@ connectDB();
 
 app.use('/api/auth', authRoutes);
 app.use('/api/child', childRoutes);
+app.use("/api/room", roomRoutes);
+app.use('/api/detection', emotionDetectionRoutes);
 app.use('/api/deepgram', deepgramRoutes); // Register Deepgram routes
 
 app.listen(port, () => {
