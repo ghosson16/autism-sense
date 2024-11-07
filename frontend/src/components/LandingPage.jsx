0360f42c0a32f0b42489403c6f5b8ae6e22eea12
ghosson16 <ghossonmoafa2002@gmail.com>
2024-11-07 15:48:08 +0300

    update landing page

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -50,57 +50,46 @@ const LandingPage = () => {
   return (
     <div className="landing-container">
       <div className="logo-container"></div>
-      <br /> <br /> <br />
+      <br />
 
       <section className="button-container">
-        <button
-          className="btn therapist-button"
-          onClick={() => navigate("/host")}
-          style={{ display: "flex", alignItems: "center", gap: "10px", padding: "10px" }}
-        >
+        <button className="btn therapist-button" onClick={() => navigate("/host")}>
           <lord-icon
             src="https://cdn.lordicon.com/uwixxspd.json"
             trigger="morph"
             colors="primary:#000000,secondary:#3a3347,tertiary:#faddd1,quaternary:#e5e0e9,quinary:#545454,senary:#87ceed"
-            style={{ width: "140px", height: "140px" }}
             className="therapist-icon"
+            style={{ width: "140px", height: "140px" }}
           ></lord-icon>
           I'M A THERAPIST
         </button>
 
-        <button
-          className="btn child-button"
-          onClick={handleSignInUp}
-          style={{ display: "flex", alignItems: "center", gap: "10px", padding: "10px" }}
-        >
+        <button className="btn child-button" onClick={handleSignInUp}>
           <lord-icon
             src="https://cdn.lordicon.com/cfbvvbil.json"
             trigger="morph"
             colors="primary:#000000,secondary:#faddd1,tertiary:#3a3347,quaternary:#f28ba8,quinary:#f49cc8"
-            style={{ width: "140px", height: "140px" }}
             className="child-icon"
+            style={{ width: "140px", height: "140px" }}
           ></lord-icon>
           I'M A CHILD
         </button>
       </section>
       <br />
       {showAuthModal && <AuthModal onClose={closeAuthModal} />}
-      <br /> <br /> <br />
+      <br />
 
       {/* Footer Section */}
       <div className="footer-container">
-        <div
-          className="footer-section about"
-          style={{ textAlign: "center" }}
-          ref={(el) => (footerSectionsRef.current[0] = el)}
-        >
+        <div className="footer-section about" ref={(el) => (footerSectionsRef.current[0] = el)}>
           <lord-icon
             src="https://cdn.lordicon.com/ofzpbawy.json"
             trigger="loop"
             delay="4000"
             stroke="bold"
             colors="primary:#121331,secondary:#ebe6ef,tertiary:#e4e4e4,quaternary:#87ceed"
-            style={{ width: "100px", height: "100px", marginBottom: "10px" }}
+            className="footer-icon"
+            style={{ width: "140px", height: "140px" }}
           ></lord-icon>
           <h2>Community & Inclusion</h2>
           <p>
@@ -108,18 +97,15 @@ const LandingPage = () => {
           </p>
         </div>
 
-        <div
-          className="footer-section mission"
-          style={{ textAlign: "center" }}
-          ref={(el) => (footerSectionsRef.current[1] = el)}
-        >
+        <div className="footer-section mission" ref={(el) => (footerSectionsRef.current[1] = el)}>
           <lord-icon
             src="https://cdn.lordicon.com/fmjvulyw.json"
             trigger="loop"
             delay="4000"
             stroke="bold"
             colors="primary:#121331,secondary:#ffffff,tertiary:#3a3347,quaternary:#87ceed,quinary:#f9c9c0,senary:#f24c00"
-            style={{ width: "100px", height: "100px", marginBottom: "10px" }}
+            className="footer-icon"
+            style={{ width: "140px", height: "140px" }}
           ></lord-icon>
           <h2>Our Vision</h2>
           <p>
@@ -127,18 +113,15 @@ const LandingPage = () => {
           </p>
         </div>
 
-        <div
-          className="footer-section features"
-          style={{ textAlign: "center" }}
-          ref={(el) => (footerSectionsRef.current[2] = el)}
-        >
+        <div className="footer-section features" ref={(el) => (footerSectionsRef.current[2] = el)}>
           <lord-icon
             src="https://cdn.lordicon.com/eewfjpmj.json"
             trigger="loop"
             delay="4000"
             stroke="bold"
             colors="primary:#121331,secondary:#ffffff,tertiary:#e8e230,quaternary:#9cc2f4"
-            style={{ width: "100px", height: "100px", marginBottom: "10px" }}
+            className="footer-icon"
+            style={{ width: "140px", height: "140px" }}
           ></lord-icon>
           <h2>Key Features</h2>
           <p className="features-list">
