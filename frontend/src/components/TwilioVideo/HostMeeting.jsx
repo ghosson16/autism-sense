91dcec26a24bf6904f72c8c8917a00bb3b6c2d98
rareemi <121254688+rareemi@users.noreply.github.com>
2024-11-08 20:53:05 +0300

    reverted

--- frontend/src/components/TwilioVideo/HostMeeting.jsx
+++ frontend/src/components/TwilioVideo/HostMeeting.jsx
@@ -55,39 +55,38 @@ const HostMeeting = () => {
             </div>
 
            
-            <h2 style={{ textAlign: "center", margin: 0 }}>Quick Tips Before you start!</h2> <br/>
+            <h2 className="modal-header">Quick Tips Before you start!</h2> 
+            <br />
 
 
             {/* Instructions List */}
-            <ol style={{ lineHeight: "1.7", padding: "10px 0", textAlign: "justify" }}>
-              <li style={{ marginBottom: "15px", }}>
-                <strong>Create & Share the Meeting Link:
-                  <br/>
-                  </strong> Start the meeting and share the link with the child or their caregiver.
-              </li>
-              <li style={{ marginBottom: "15px" }}>
-                <strong>Emotion Detection:</strong>
-                <br/>
-                 Emotions will appear as icons based on your expressions; encourage the child to observe and relate these icons.
-              </li>
-              <li style={{ marginBottom: "15px" }}>
-                <strong>Guided Help:</strong>
-                <br/>
-                 If the child is struggling, remind them to use the Help button for response suggestions.
-              </li>
-              <li style={{ marginBottom: "15px" }}>
-                <strong>Emotion Game:</strong> 
-                <br/>The child can start an emotion-identification game, providing a fun, interactive learning experience.
-              </li>
-            </ol>
+            <ol className="instructions-list">
+              <li>
+                <strong>Create & Share the Meeting Link:</strong><br />
+                Start the meeting and share the link with the child or their caregiver.
 
-       
-            <button
-              className="start-button"
-              onClick={initiateMeeting}
-            >
-              Start Meeting
-            </button>
+              </li>
+              <li>
+                <strong>Emotion Detection:</strong><br />
+                Emotions will appear as icons based on your expressions; encourage the child to observe and relate these icons.
+
+              </li>
+              <li>
+                <strong>Guided Help:</strong><br />
+                If the child is struggling, remind them to use the Help button for response suggestions.
+
+              </li>
+              <li>
+              <strong>Emotion Game:</strong><br />
+    The child can start an emotion-identification game, providing a fun, interactive learning experience.
+  </li>
+</ol>
+
+{/* Move the Start Meeting button outside the ol list */}
+<button className="start-button" onClick={initiateMeeting}>
+  Start Meeting
+</button>
+<br />
             <br />
           </div>
         </div>
